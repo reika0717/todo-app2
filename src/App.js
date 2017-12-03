c786b6159f51f3cc2c38264c777b0258ef9e4a39
reika hirahara <31158474+reika0717@users.noreply.github.com>
2017-12-03 23:46:33 +0900

    exist strikethrough when you click task name

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
 import React, { Component } from 'react';
 import MyForm from './components/MyForm'
+import v4 from 'uuid/v4'
 
 class App extends Component {
   constructor(props){
     super(props)
     this.state = {
-      task: 'task1'
+      id:v4(),
+      description: 'task1',
+      completed: false
     }
   }
 
   render() {
+    const {description, completed} = this.state
     return (
       <div>
         <MyForm />
         <ul>
-          <li>{this.state.task}</li>
+          <li
+            onClick={()=>{this.setState({completed: !completed})}}
+            style={{textDecoration: completed ? 'line-through' : 'none'}}
+            >{description}</li>
         </ul>
       </div>
     );
