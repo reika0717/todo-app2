2e79d28f0aeb673b9c20816a9c4f2e1ecb5b867e
reika hirahara <31158474+reika0717@users.noreply.github.com>
2017-12-03 23:41:18 +0900

    display task name

--- src/App.js
+++ src/App.js
@@ -2,10 +2,20 @@ import React, { Component } from 'react';
 import MyForm from './components/MyForm'
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      task: 'task1'
+    }
+  }
+
   render() {
     return (
       <div>
         <MyForm />
+        <ul>
+          <li>{this.state.task}</li>
+        </ul>
       </div>
     );
   }
